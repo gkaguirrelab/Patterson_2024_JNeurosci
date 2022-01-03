c31537c7e2bf81374f1c099dd5fa2580315731fb
gkaguirre <gkaguirre@me.com>
2022-01-03 11:29:39 -0500

    Updated to final, integrated analysis approach

--- code/analysis/downloadResultsFromFlywheel.m
+++ code/analysis/downloadResultsFromFlywheel.m
@@ -7,25 +7,26 @@ localSaveDir = getpref('mriSinaiAnalysis','localSaveDir');
 % analysis IDs
 subjectNames = {'HEROgka1','HEROasb1'};
 shortNames = {'gka','asb'};
-analysisLabels = {'L-M','S','LF'};
-analysisIDs = { {'60e9ea50a85492ed8f96cabd','60e9ea334ef89230db2b7021','60e9ea6dbceb4c0bc9e0767e'} , ...
-    {'60e9eaa1a74445f40c56b123', '60e9ea85bd00f64426dd9301','60e9eabf4ef89230db2b7027'} };
+analysisIDs = { '6117d4db18adcc19d6e0f820','611d158fa296f805e7a2da75' };
+
+% List of the output files to download
+desiredOutputs = {'fig1.pdf','fig2.pdf','maps_cifti.zip','mtSinai_results.mat'};
 
 % Create a location to save the retino files
 retinoSaveDir = fullfile(localSaveDir,'retinoFiles');
 mkdir(retinoSaveDir);
 
-% Create a flywheel object. You need to set you flywheelAPIKey in the
+% Create a flywheel object. You need to set your flywheelAPIKey in the
 % "flywheelMRSupport" local hook.
 fw = flywheel.Flywheel(getpref('flywheelMRSupport','flywheelAPIKey'));
 
 % Download and unzip the retino maps
-retinoMapID = '5dc88aaee74aa3005e169380';
-retinoFileName = 'TOME_3021_cifti_maps.zip';
-tmpPath = fullfile(retinoSaveDir,retinoFileName);
-fw.downloadOutputFromAnalysis(retinoMapID,retinoFileName,tmpPath);
-command = ['unzip -q -n '  escapeFileCharacters(tmpPath) ' -d ' escapeFileCharacters(retinoSaveDir)];
-system(command);
+% retinoMapID = '5dc88aaee74aa3005e169380';
+% retinoFileName = 'TOME_3021_cifti_maps.zip';
+% tmpPath = fullfile(retinoSaveDir,retinoFileName);
+% fw.downloadOutputFromAnalysis(retinoMapID,retinoFileName,tmpPath);
+% command = ['unzip -q -n '  escapeFileCharacters(tmpPath) ' -d ' escapeFileCharacters(retinoSaveDir)];
+% system(command);
 
 % Create a location to save the results files
 resultsSaveDir = fullfile(localSaveDir,'resultsFiles');
@@ -33,15 +34,18 @@ mkdir(resultsSaveDir);
 
 % Loop through the subjects and directions and download results.mat
 for ss = 1:2
-        
-    for dd = 1:3
-        
-        % Download the results file for this subject / direction
-        fileStem = [subjectNames{ss} '_agtcOL_'];
-        fileName = [fileStem 'results.mat'];
-        tmpPath = fullfile(resultsSaveDir,[analysisLabels{dd} '_' fileName]);
-        fw.downloadOutputFromAnalysis(analysisIDs{ss}{dd},fileName,tmpPath);
-        
+
+    fileStem = [subjectNames{ss} '_'];
+
+    % Create a directory for this subject
+    resultsSaveDir = fullfile(localSaveDir,[fileStem 'resultsFiles']);
+    mkdir(resultsSaveDir);
+
+    % Download the results file for this subject
+    for ff = 1:length(desiredOutputs)
+    fileName = [fileStem desiredOutputs{ff}];
+    tmpPath = fullfile(resultsSaveDir,fileName);
+    fw.downloadOutputFromAnalysis(analysisIDs{ss},fileName,tmpPath);
     end
+
 end
-        

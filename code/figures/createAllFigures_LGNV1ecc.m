a464fd20442c9e570d5271b3036a9442714305bc
gkaguirre <gkaguirre@me.com>
2022-04-23 10:21:40 -0400

    Add some more figure entries

--- code/figures/createAllFigures_LGNV1ecc.m
+++ code/figures/createAllFigures_LGNV1ecc.m
@@ -3,10 +3,11 @@
 % midget wiring"
 
 % MRI pre-processing
-
+% The directory submitGears contains CSV files that provide the parameters
+% used to process the raw fMRI data
 
 % Figure 2 - example MRI data
-
+run averageTimeSeries.m % Downloads Flywheel Results files, creates plots of average V1 time series for each subject
 
 % Figure 3 - full field LGN and V1 TSFs, peak response, and peak frequency
 run ExtractTTFdataDifExpFitArea.m % downloads MRI data from flywheel, and calculates bootstrapped fits for TSFs in fullfield LGN and V1, and saves them
@@ -17,3 +18,4 @@ run ExtractTTFdataDifExpFitEccentricity.m % downloads MRI data from flywheel, an
 run TTF_figures_eccExp.m % creates TSF figure, and peak response and peak frequency
 
 % Figure 6 - midget amplification model
+run midgetMixingSimulation.m % Runs Wool 2018 DoG code, then create Figure 6

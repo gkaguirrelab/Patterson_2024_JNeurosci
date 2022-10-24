156e7bc1081bb98ffb5f92a4c73055f9a380a756
gkaguirre <gkaguirre@me.com>
2022-10-24 15:24:22 -0400

    Improve the bistratified cell density model

--- code/gkaTemporalModel/stages/returnPostRetinalRF.m
+++ code/gkaTemporalModel/stages/returnPostRetinalRF.m
@@ -34,6 +34,26 @@ end
 % eccentricity
 midgetFraction = @(eccDeg) 0.8928*(1+eccDeg/41.03).^(-1);
 
+% Bistratified RGCs as a function of eccentricity. Values taken from Figure
+% 13B of:
+%   Dacey DM. Morphology of a small-field bistratified ganglion cell type
+%   in the macaque and human retina. Visual neuroscience. 1993
+%   Nov;10(6):1081-98.
+% Further, set the bistratified density to 0 below 0.5 degrees to model the
+% tritanopic foveola.
+%{
+    daceyXmm = [1.0082    1.9945    2.9589    3.9671    4.9534    5.9616    6.9699    7.9123    8.9205    9.9507   10.9589   11.9671    12.9973   13.9616   14.9699];
+    daceyY = 0.01 .* [1.3912    1.5573    2.0519    2.7377    3.2223    3.4308    3.5179    3.7927    4.1928    4.7528    5.3876    5.8085  5.8085    6.1072    6.7515];
+    daceyXdeg = 0.1 + 3.4.*daceyXmm + 0.035.*daceyXmm.^2;
+    pp = polyfit(daceyXdeg,daceyY,2)
+    bistratifiedFraction = @(eccDeg) (eccDeg>0.5).*( pp(1).*eccDeg.^2+pp(2).*eccDeg+pp(3));
+    figure
+    semilogy(daceyXdeg,daceyY,'ok'); hold on
+    semilogy(0:1:90,bistratifiedFraction(0:1:90),'-r')
+%}
+pp(1) = -0.000002818937309; pp(2) = 0.001127464065019; pp(3) = 0.009832178782458;
+bistratifiedFraction = @(eccDeg) (eccDeg>0.5).*( pp(1).*eccDeg.^2+pp(2).*eccDeg+pp(3));
+
 % At each eccentricity, what fraction of the total number of RGCs is of a
 % given cell class? We simply assume that the bistratified RGCs make up a
 % uniform 10% of all RGCs across eccentricity
@@ -41,9 +61,9 @@ switch cellClass
     case 'midget'
         proportionFunc = @(ecc) midgetFraction(ecc);
     case 'parasol'
-        proportionFunc = @(ecc) 0.9-midgetFraction(ecc);
+        proportionFunc = @(ecc) 1.0-midgetFraction(ecc)-bistratifiedFraction(ecc);
     case 'bistratified'
-        proportionFunc = @(ecc) 0.1;
+        proportionFunc = @(ecc) bistratifiedFraction(ecc);
 end
 
 % Loop over the meridians and obtain the RGCf density functions

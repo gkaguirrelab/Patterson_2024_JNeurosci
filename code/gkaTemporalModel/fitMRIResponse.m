4b8022b5352ac670e7e85a2e4a9282b3070b2341
gkaguirre <gkaguirre@me.com>
2022-10-28 17:17:50 -0400

    Updated after search

--- code/gkaTemporalModel/fitMRIResponse.m
+++ code/gkaTemporalModel/fitMRIResponse.m
@@ -2,84 +2,58 @@ function [pMRI,fVal] = fitMRIResponse(p0,stimulusDirections,studiedEccentricites
 % Fit the RGC-referred temporal model to combined V1 and LGN data
 %
 % Syntax:
-%  [p,fVal] = fitMRIResponseData(p0, v1FreqX, v1Eccentricity, v1Y, v1W, lgnFreqX, lgnY, lgnW, whichModel, useMonotonicConstraint)
+%  [p,fVal] = fitMRIResponseData(p0,stimulusDirections,studiedEccentricites,studiedFreqs,v1Y,v1W,lgnY,lgnW,useMonotonicConstraint)
 %
 % Description:
 %   Model that simultaneously fits TTF responses from the LGN, and from V1
-%   across eccentricity for a particular post-receptoral direction. The
-%   model starts with Fourier-domain models of the chromatic and achromatic
+%   across eccentricity for multiple post-receptoral directions. The model
+%   starts with Fourier-domain models of the chromatic and achromatic
 %   temporal sensitivity functions of the RGCs as a function of
 %   eccentricity. Responses at the LGN are modeled as the RGC responses,
 %   subject to a delayed surround inhibition. Responses at V1 are the
 %   responses from the LGN, subject to another iteration of delayed
 %   surround inhibition, and also subject to a second-order, low-pass
-%   temporal filter.
+%   temporal filter. Different sets of parameters modify the chromatic and
+%   achromatic signals arriving from the LGN.
 %
-%   The effect of eccentricity is present in the model in
-%   both a fixed and parameterized form. There is a fixed effect of
-%   eccentricity at the retina, which is derived from the change in the
-%   center and surround cone inputs to the midget RGCs, and by changes in
-%   the parameters of the temporal model used to fit the empirical RGC
-%   data. The effect of eccentricity in the post-retinal model is found in
-%   the varying of the index of surround inhibition.
-%
-%   There is one unique parameter of the model, shared across all cell
-%   types and eccentricites:
-%       LMConeRatio   - Ratio of L-to-M cones in the modeled retina
-%
-%   There are four parameters that are "fixed" for each cell type across
-%   eccentricities:
-%
-%       lgnGain       - Multiplicative gain for fitting the LGN amplitudes
-%       secondOrderFc - Corner frequency of 2nd order filter at V1 stage
-%       secondOrderQ  - "Quality" parameter of 2nd order filter at V1
-%       surroundDelay - The delay (in msecs) between the center and
-%                       surround, used at both the LGN and V1 stages
-%
-%   and, there are two parameters that "float" across both cell type and
-%   eccentricity:
-%       surroundIndex - The index (range 0 - 1) of the surround inhibition
-%                       at the LGN and V1 stages. One value for each of the
-%                       k eccentricity measurements from V1
-%       v1gain        - Multiplicative gain for fitting the V1 amplitudes
-%                       at each eccentricity band.
+%   The effect of eccentricity is present in the model in both a fixed and
+%   parameterized form. There is a fixed effect of eccentricity at the
+%   retina, which is derived from the change in the center and surround
+%   cone inputs to the midget RGCs, and by changes in the parameters of the
+%   temporal model used to fit the empirical RGC data. The effect of
+%   eccentricity in the post-retinal model is found in the varying of the
+%   index of surround inhibition, and the gain parameter that scales the
+%   resulting temporal sensitivity function to the observed BOLD fMRI data.
 %
 % Inputs:
-%   p0                    - 1x[c*(4+k*2)] vector of parameters for the
+%   p0                    - 1x[2+c+6+s*k*2] vector of parameters for the
 %                           model fit, where c is the number of cell
-%                           classes (midget, parasol, bistratified)
-%   v1FreqX               - 1x(s*n) vector of frequency values (in Hz) the
-%                           correspond to the cortical amplitude values in
-%                           v1Y. The length is equal to s*j*k, where s is
-%                           the number of stimulusDirections
-%                           ('LminusM','S','LMS'), j is the number of
-%                           frequencies and k is the number of
-%                           eccentricities.
-%   v1Eccentricity        - 1x(s*n) vector of eccentricity locations (in
-%                           degrees) from which the v1Y measurements were
-%                           made.
-%   v1Y                   - 1x(s*n) vector of BOLD amplitudes at each of
+%                           classes (midget, parasol, bistratified), s is
+%                           the number of stimulus directions (LminusM, S,
+%                           LMS), and k is the number of eccentricities.
+%   stimulusDirections    - 1xs cell array naming the stimulus directions,
+%                           such as {'LminusM','S','LMS'}
+%   studiedEccentricites  - 1xk array of the centers of the studied
+%                           eccentricity bins.
+%   studiedFreqs          - 1xn vector of frequency values (in Hz). 
+%   v1Y                   - 1x(s*k*n) vector of BOLD amplitudes at each of
 %                           many eccentricities and stimulus frequencies.
 %                           The order of these is:
-%                             [ [s1e1f1 s1e1f2 ... s1e1fj] [s1e2f1 s1e2f2 ... s1e2fj]
-%   v1W                   - 1x(s*n) vector of weights for each of the
+%                             [ [s1e1f1 s1e1f2 ... s1e1fj] [s1e2f1 s1e2f2 ... s1e2fn]
+%   v1W                   - 1x(s*k*n) vector of weights for each of the
 %                           measurements in v1Y. One choice is the inverse
 %                           of the 95% CI of the measurement.
-%   lgnFreqX              - 1x(s*j) vector of frequencies for which the LGN
-%                           measurements were made.
-%   lgnY                  - 1x(s*j) vector of BOLD amplitudes measured from
+%   lgnY                  - 1x(s*n) vector of BOLD amplitudes measured from
 %                           the LGN.
-%   lgnW                  - 1x(s*j) vector of weights for the measurements
+%   lgnW                  - 1x(s*n) vector of weights for the measurements
 %                           in lgnY.
-%   stimulusDirections    - Cell array naming the stimulus directions, such
-%                           as {'LminusM','S','LMS'}
 %   useMonotonicConstraint - Logical. Controls if the model includes a
 %                           non-linear constraint that requires the
 %                           surround index to decrease in value across
 %                           eccentricity positions for a given cell class.
 %
 % Outputs:
-%   p                     - 1x[c*(4+k*2)] vector of model fit parameters
+%   p                     - 1x[2+c+6+s*k*2] vector of model fit parameters
 
 % Load the RGC model parameters
 loadPath = fullfile(fileparts(fileparts(fileparts(mfilename('fullpath')))),'data','temporalModelResults','rgcTemporalModel.mat');
@@ -90,15 +64,17 @@ nEcc = length(studiedEccentricites);
 nUniqueParams = 11;
 nFixedParams = 0;
 
-% The model includes parameters for each of the cell classes
-postReceptoralPaths = {'midget.LminusM','parasol.LMS','bistratified.S','midget.LMS'};
+% The model includes parameters for each of the "post receptoral paths",
+% which is realized here as the interaction of retinal ganglion cell
+% classes with stimulus directions.
+postReceptoralPaths = {'midget.LminusM','bistratified.S','parasol.LMS','midget.LMS'};
 
 % Set up the parameters.
 
 % lgn parameters, organized as
-% - lgn surround delay
-% - lgn surround index
-% - lgn gain (varies by midget, bistratified, parasol)
+% - lgn surround delay (msecs)
+% - lgn surround index (proportion, in the range 0 - 1)
+% - lgn gain (a.u.; varies by midget, bistratified, parasol)
 lb =  [05 0.1 repmat(0000,1,3)];
 plb = [10 0.3 repmat(0.01,1,3)];
 pub = [15 0.6 repmat(0.10,1,3)];
@@ -115,8 +91,8 @@ for pp = 1:2
     ub =  [ ub 90 0.7 40];
 end
 
-% v1 paramters that vary by eccentricity, organized as LminusM, S,
-% LMS-parasol, LMS-midget:
+% v1 paramters that vary by eccentricity, organized as:
+% LminusM, S, LMS-parasol, LMS-midget:
 % - surround index (a.u.)
 % - surround gain (a.u.)
 for pp = 1:length(postReceptoralPaths)
@@ -129,10 +105,7 @@ end
 % Returns the TTF, and handles reshaping into a linear vector
 myV1TTF = @(pMRI) assembleV1ResponseAcrossStimsAndEcc(pMRI,stimulusDirections,studiedEccentricites,studiedFreqs,rgcTemporalModel,nUniqueParams,nFixedParams);
 myLGNTTF = @(pMRI) assembleLGNResponseAcrossStims(pMRI,stimulusDirections,studiedFreqs,rgcTemporalModel);
-
-% Shrink penalty that attempts to match various parameters
-% myShrinkPenalty = @(pMRI) calculateShrinkPenalty(pMRI,shrinkScaleFactor);
-
+        
 % The weighted objective
 myObj = @(pMRI) norm(v1W.*(v1Y - myV1TTF(pMRI))) + ...
     norm(lgnW.*(lgnY - myLGNTTF(pMRI)));

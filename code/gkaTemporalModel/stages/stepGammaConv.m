4e3f99200a87a859f321c091fcd8712c75c24612
gkaguirre <gkaguirre@me.com>
2022-11-22 13:10:40 -0500

    Deprecated

--- code/gkaTemporalModel/stages/stepGammaConv.m
+++
@@ -1,9 +0,0 @@
-function irfOut = stepGammaConv(irfIn,deltaT,t1,w)
-
-gammaTime = 0:deltaT:1;
-gammaKernel = gammaTime.*exp(-gammaTime/t1)-w.*gammaTime.*exp(-gammaTime/(1.5*t1));
-gammaKernel = gammaKernel / sum(gammaKernel);
-temp = conv(irfIn,gammaKernel);
-irfOut = temp(1:end-length(gammaKernel)+1);
-
-end

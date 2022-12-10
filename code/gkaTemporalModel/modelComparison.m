20fce0d079040cf6cb75ec3614f05a280392932f
gkaguirre <gkaguirre@me.com>
2022-12-10 08:21:57 -0500

    iter

--- code/gkaTemporalModel/modelComparison.m
+++ code/gkaTemporalModel/modelComparison.m
@@ -9,35 +9,38 @@ mriData = loadMRIResponseData();
 % Some parameters
 subjects = {'gka','asb'};
 stimulusDirections = {'LminusM','S','LMS'};
-modelTypes = {'stimulus','cell','mix'};
+modelTypes = {'stimulus','cell'};
 
-nBoots = 11;
+nBoots = 5;
 
 data = nan(length(subjects),length(modelTypes),nBoots);
 
 for ss = 1:length(subjects)
 
     % Loop over the model types
-    for cc = 1:length(modelTypes)
 
         % Get the list of result files for this subject
-        resultList = dir(fullfile(dataPath,'temporalModelResults',modelTypes{cc},subjects{ss},'mriTemporalModel_*.mat'));
+        resultList = dir(fullfile(dataPath,'temporalModelResults','cell',subjects{ss},'mriTemporalModel_*.mat'));
 
         % Load each result, and save the fVal
         for rr=1:length(resultList)
+            % Load the cell result
             loadPath = fullfile(resultList(rr).folder,resultList(rr).name);
             load(loadPath,'mriTemporalModel');
-            data(ss,cc,rr)=mriTemporalModel.(subjects{ss}).fVal;
+            data(ss,2,rr)=mriTemporalModel.(subjects{ss}).fVal;
+
+            % Load the stimulus result
+            loadPath = strrep(loadPath,'/cell/','/stimulus/');
+            load(loadPath,'mriTemporalModel');
+            data(ss,1,rr)=mriTemporalModel.(subjects{ss}).fVal;
         end
 
-    end
+    data(data==0)=nan;
 
     % Report the comparisons
     [~,p,~,stats] = ttest(squeeze(data(ss,1,:)),squeeze(data(ss,2,:)));
     fprintf([subjects{ss} ': stimulus - cell, t(df) = %2.2f (%d), p = %2.2e \n'],stats.tstat,stats.df,p)
 
-    [~,p,~,stats] = ttest(squeeze(data(ss,1,:)),squeeze(data(ss,3,:)));
-    fprintf([subjects{ss} ': stimulus - mix, t(df) = %2.2f (%d), p = %2.2e \n'],stats.tstat,stats.df,p)
 
 end
 

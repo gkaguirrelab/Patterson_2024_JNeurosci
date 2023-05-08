db9a9c59b9223ece4037748aee7fabb54ee95c21
gkaguirre <gkaguirre@me.com>
2023-05-08 07:48:04 -0400

    A sandwhich model (compressive summation) for the TTFs

--- code/gkaTemporalModel/cstPlayground.m
+++ code/gkaTemporalModel/cstPlayground.m
@@ -2,28 +2,11 @@
 %% Housekeeping
 clear
 close all
-verbose = false;
-
-
-%% Pick a subject, stimulus, and eccentricity band
-whichSub = 1;
-
-        % Options. Indicate that the objective function is deterministic, and
-        % handle verbosity
-        optionsBADS.UncertaintyHandling = 0;
-        if verbose
-            optionsBADS.Display = 'iter';
-        else
-            optionsBADS.Display = 'off';
-        end
-
-        % The optimization toolbox is currently not available for Matlab running
-        % under Apple silicon. Detect this case and tell BADS so that it doesn't
-        % issue a warning
-        V = ver;
-        if ~any(strcmp({V.Name}, 'Optimization Toolbox'))
-            optionsBADS.OptimToolbox = 0;
-        end
+verbose = true;
+searchFlag = false;
+nSearches = 1;
+sortParamsPriorToSearch = false;
+useMonotonicPenalty = false;
 
 %% Load the data and basic model elements
 
@@ -34,46 +17,88 @@ mriData = loadMRIResponseData();
 rgcTemporalModel = fitRGCFResponse(false,false);
 
 % Define the eccentricity locations of the data. We use the log-mid point
-% within each of the bins for the cortical
+% within each of the V1 cortical bins
 nEccs = 6;
 eccDegBinEdges = logspace(log10(0.7031),log10(90),15);
-studiedEccentricitesFull = eccDegBinEdges(4:2:14);
+studiedEccentricites = eccDegBinEdges(4:2:14);
 
 % The identities of the stims and subjects
 subjects = {'gka','asb'};
 stimulusDirections = {'LminusM','S','LMS'};
 plotColor = {'r','b','k'};
-nSubjects = 2;
-nStims = 3;
-nCells = 3;
+nSubs = length(subjects);
+nStims = length(stimulusDirections);
 
-
-% The number of acquisitions obtained for each measurement
+% The number of acquisitions obtained for each measurement. Might want this
+% if we are going to do some boot-strapping
 nAcqs = 12;
+
+% Fixed features of the model
+nCells = 3; nParams = 3;
 
 % The frequencies studied
 studiedFreqs = [2 4 8 16 32 64];
 
-for whichSub = 1:nSubjects
+% BADs Options. Indicate that the objective function is deterministic, and
+% handle verbosity
+optionsBADS.UncertaintyHandling = 0;
+if verbose
+    optionsBADS.Display = 'iter';
+else
+    optionsBADS.Display = 'off';
+end
 
-    figure
+% The optimization toolbox is currently not available for Matlab running
+% under Apple silicon. Detect this case and tell BADS so that it doesn't
+% issue a warning
+V = ver;
+if ~any(strcmp({V.Name}, 'Optimization Toolbox'))
+    optionsBADS.OptimToolbox = 0;
+end
 
-    parfor eccIdx = 1:6
-        nEccs = 1;
-        studiedEccentricites = studiedEccentricitesFull(eccIdx);
-   
-        Y = zeros(3,1,length(studiedFreqs));
-        W = zeros(3,1,length(studiedFreqs));
+% Loop over optimizations
+for nn = 1:nSearches
 
-        % Get the data
-        for whichStim = 1:nStims
-            %            for ee = length(eccIdx)
-            ee = 1;
-            whichEcc = ee; %eccIdx(ee);
-            thisMatrix = mriData.(subjects{whichSub}).(stimulusDirections{whichStim}).(['v1_ecc' num2str(whichEcc)]);
-            Y(whichStim,ee,:) = mean(thisMatrix);
-            W(whichStim,ee,:) = 1./std(thisMatrix);
+    % Loop over subjects
+    for whichSub = 1:nSubs
+
+        % Load the data
+        Y = zeros(nStims,nEccs,length(studiedFreqs));
+        W = zeros(nStims,nEccs,length(studiedFreqs));
+        for eccIdx = 1:nEccs
+            for whichStim = 1:nStims
+                thisMatrix = mriData.(subjects{whichSub}).(stimulusDirections{whichStim}).(['v1_ecc' num2str(eccIdx)]);
+                Y(whichStim,eccIdx,:) = mean(thisMatrix);
+                W(whichStim,eccIdx,:) = 1./std(thisMatrix);
+            end
         end
+
+        % We double the weights on the light-flux fits
+        W(:,3,:) = W(:,3,:)*2;
+
+        % If there was a prior analysis start with the last result
+        load('cstResults.mat','results')
+
+        % Use the prior solution as the p0 guess
+        if isfield(results,subjects{whichSub})
+            p0 = results.(subjects{whichSub}).p;
+        else
+            p0 = results.avgSub.p;
+        end
+
+        % Force the low-pass filter frequencies to be in descending order
+        if sortParamsPriorToSearch
+            k = reshape(p0(2:end),nParams,nCells,nEccs);
+            for cc = 1:3
+                k(1,cc,:) = sort(squeeze(k(1,cc,:)),'descend');
+                k(2,cc,:) = sort(squeeze(k(2,cc,:)),'descend');
+            end
+            p0(2:end) = reshape(k,1,nParams*nCells*nEccs);
+        end
+
+        % Bounds on Q, corner frequency, exponentiation, gain
+        lb = [1.0 repmat([ 5, 0.4, 0.01],1,nCells*nEccs)];
+        ub = [1.8 repmat([45, 2.5,  100],1,nCells*nEccs)];
 
         % Define the response function
         myResponseMatrix = @(p) returnResponse(p,stimulusDirections,studiedEccentricites,studiedFreqs,rgcTemporalModel);
@@ -82,59 +107,84 @@ for whichSub = 1:nSubjects
         vectorize = @(x) x(:);
 
         % Define the objective
-        myObj = @(p) norm(vectorize(W).*(vectorize(Y) - vectorize(myResponseMatrix(p))));
+        if useMonotonicPenalty
+            myObj = @(p) norm(vectorize(W).*(vectorize(Y) - vectorize(myResponseMatrix(p)))) + ...
+                calcMonotonicPenalty(p,nParams,nCells,nEccs);
+        else
+            myObj = @(p) norm(vectorize(W).*(vectorize(Y) - vectorize(myResponseMatrix(p))));
+        end
 
-        % No non-linear constraint at the moment
+        % The place to define a non-linear constraint
         myNonbcon = [];
 
-        %% Set the bounds
-        % Accelerating non-linearity
-        % Q parameter
-        % Vector of corner frequencies across eccentricity for each cell class
-        % Vector of gains for each stimulus class
-        lb =  [1.5,0.6,0.6,0.6,  ...
-            repmat(5,1,nEccs), repmat(5,1,nEccs), repmat(10,1,nEccs), ...
-            repmat(0.01,1,nCells*nEccs)];
-        ub =  [1.8,2.2,1.2,1.2, ...
-            repmat(30,1,nEccs), repmat(15,1,nEccs), repmat(45,1,nEccs), ...
-            repmat(100,1,nCells*nEccs)];
-
-
-        %% Set the p0
-        switch subjects{whichSub}
-            case 'gka'
-                p0 =  [1.5,1.5,1,1, ...
-                    repmat(30,1,nEccs), repmat(10,1,nEccs), repmat(35,1,nEccs), ...
-                    repmat(0.1,1,nEccs), repmat(5,1,nEccs), repmat(1,1,nEccs)];
-            case 'asb'
-                p0 =  [1.5,1.5,1,1, ...
-                    repmat(30,1,nEccs), repmat(10,1,nEccs), repmat(35,1,nEccs), ...
-                    repmat(0.1,1,nEccs), repmat(5,1,nEccs), repmat(1,1,nEccs)];
+        % BADS it
+        if searchFlag
+            [p,fVal] = bads(myObj,p0,lb,ub,[],[],myNonbcon,optionsBADS);
+        else
+            p=p0;
+            fVal = myObj(p0);
         end
 
-        % BADS it
-        [p,fVal] = bads(myObj,p0,lb,ub,[],[],myNonbcon,optionsBADS);
-
-        % Store it
-        results(eccIdx).(subjects{whichSub}).p = p;
-        results(eccIdx).(subjects{whichSub}).fVal = fVal;
-
-        % Get the response
+        % Get the response matrix
         yFit = myResponseMatrix(p);
 
+        % Store it
+        results.(subjects{whichSub}).p0 = p0;
+        results.(subjects{whichSub}).p = p;
+        results.(subjects{whichSub}).fVal = fVal;
+        results.(subjects{whichSub}).Y = Y;
+        results.(subjects{whichSub}).W = W;
+        results.(subjects{whichSub}).yFit = yFit;
+
+        % Save it
+        if searchFlag
+            save('cstResults.mat','results')
+        end
+
         % Plot it
-        for ss=1:nStims
-            subplot(nStims,6,(ss-1)*6 + eccIdx)
-            plot(squeeze(Y(ss,1,:)),'.k');
-            hold on
-            plot(squeeze(yFit(ss,1,:)),['-' plotColor{ss}]);
+        figure
+        Y = results.(subjects{whichSub}).Y;
+        yFit = results.(subjects{whichSub}).yFit;
+        for ee=1:6
+            for ss=1:nStims
+                subplot(nStims,nEccs,(ss-1)*nEccs + ee)
+                plot(squeeze(Y(ss,ee,:)),'.k');
+                hold on
+                plot(squeeze(yFit(ss,ee,:)),['-' plotColor{ss}]);
+                ylim([-2 6]);
+                refline(0,0);
+            end
         end
         drawnow
+
+        % Bode plot of the low-pass filter at the first eccentricity
+        % filterRF = stageFirstOrderLP(30,2);
+        % plotRF(filterRF);
+
     end
+
+    % Shut down the parpool to prevent memory leaks
+    poolobj = gcp('nocreate');
+    delete(poolobj);
+
+    % Plot the params
+    figure
+    subLine = {'-',':'};
+    for pp = 1:nParams
+        subplot(1,nParams,pp)
+        for whichSub = 1:nSubs
+            k = reshape(results.(subjects{whichSub}).p(2:end),3,3,6);
+            for ss = 1:nStims
+                plot(squeeze(k(pp,ss,:)),[subLine{whichSub} plotColor{ss}]);
+                hold on
+            end
+        end
+        if pp == 2
+            refline(0,1);
+        end
+    end
+
 end
-%     % Bode plot of the low-pass filter at the first eccentricity
-% filterRF = stageFirstOrderLP(30,2);
-% plotRF(filterRF);
 
 
 foo = 1;
@@ -142,12 +192,61 @@ foo = 1;
 
 %% LOCAL FUNCTIONS
 
-function [response, rfPostRetinal] = returnResponse(p,stimulusDirections,studiedEccentricites,studiedFreqs,rgcTemporalModel)
+function penalty = calcMonotonicPenalty(p,nParams,nCells,nEccs)
+% Add a penalty for non-monotonic ordering of corner frequencies across
+% eccentricity
+
+k=reshape(p(2:end),nParams,nCells,nEccs);
+
+penalty = 0;
+for cc = 1:3
+    penalty = penalty + sum(diff(squeeze(k(1,cc,:)))>0);
+    penalty = penalty + sum(diff(squeeze(k(2,cc,:)))>0);
+end
+
+penalty = penalty / 5;
+
+end
 
 
-nEccs = length(studiedEccentricites);
+
+function response = returnResponse(p,stimulusDirections,studiedEccentricites,studiedFreqs,rgcTemporalModel)
+% Assemble the response across eccentricity locations
+
+nCells = 3;
+nParams = 3;
+blockLength = nParams*nCells;
+
+parfor ee = 1:length(studiedEccentricites)
+
+    % Assemble the sub parameters
+    startIdx = (ee-1)*blockLength + 1 + 1;
+    subP = [p(1) p(startIdx:startIdx+blockLength-1)];
+
+    % Obtain the response at this eccentricity
+    responseAtEcc{ee} = returnResponseAtEcc(subP,stimulusDirections,studiedEccentricites(ee),studiedFreqs,rgcTemporalModel);
+
+end
+
+% Reshape the responses into the dimension stim x ecc x freqs
+for ee = 1:length(studiedEccentricites)
+    response(:,ee,:) = responseAtEcc{ee};
+end
+
+end
+
+
+function responseAtEcc = returnResponseAtEcc(p,stimulusDirections,eccentricity,studiedFreqs,rgcTemporalModel)
+% Obtain the modeled response at a given eccentricity
+
 nCells = 3;
 nStims = length(stimulusDirections);
+
+% The params in p are organized as:
+% - Q of 2nd-order low-pass filter
+% - Frequency (Hz) of the corner frequency of the LP filter, x nCells
+% - exponent of the non-linearity, x nCells
+% - gain, x nCells
 
 Q = p(1);
 
@@ -164,64 +263,54 @@ for ss=1:nStims
             activeCells = {'midget','parasol'};
     end
 
-    % Loop over eccentricities
-    for ee=1:nEccs
+    % Loop over cell classes for this stimulus
+    for cc=1:length(activeCells)
 
-        ecc = studiedEccentricites(ee);
+        % Get the scaling effect of stimulus contrast
+        stimulusContrastScale = returnStimulusContrastScale(activeCells{cc},stimulusDirections{ss});
 
-        % Loop over cell classes for this stimulus
-        for cc=1:length(activeCells)
+        % Get the post-retinal temporal RF
+        rfPostRetinal(cc) = returnPostRetinalRF(...
+            activeCells{cc},stimulusDirections{ss},rgcTemporalModel,...
+            eccentricity,stimulusContrastScale);
 
-            % Get the scaling effect of stimulus contrast
-            stimulusContrastScale = returnStimulusContrastScale(activeCells{cc},stimulusDirections{ss});
+        % Extract the corner frequency parameter for this eccentricity
+        % and cell class
+        idx = 1 + (cellIdx(cc)-1)*nCells + 1;
+        cornerFrequency = p(idx);
 
-            % Get the post-retinal temporal RF
-            rfPostRetinal(cc) = returnPostRetinalRF(...
-                activeCells{cc},stimulusDirections{ss},rgcTemporalModel,...
-                ecc,stimulusContrastScale);
+        % Second order low pass filter at the level of retino-
+        % geniculate synapse
+        filter = stageSecondOrderLP(cornerFrequency,Q);
+        rfPostRetinal(cc) = rfPostRetinal(cc).*filter;
 
-            % Extract the corner frequency parameter for this eccentricity
-            % and cell class
-            idx = 4 + (cellIdx(cc)-1)*nEccs + ee;
-            cornerFrequency = p(idx);
+        % Obtain the exponent for the non-linear stage
+        idx = 1 + (cellIdx(cc)-1)*nCells + 2;
+        cellExponent = p(idx);
 
-            % Second order low pass filter at the level of retino-
-            % geniculate synapse
-            filter = stageSecondOrderLP(cornerFrequency,Q);
-            %            filter = filter ./ double(subs(filter,0));
-            rfPostRetinal(cc) = rfPostRetinal(cc).*filter;
+        % Apply the non-linearity
+        scaleVal = max(abs(double(subs(rfPostRetinal(cc),0:1:100))));
+        rfPostRetinal = rfPostRetinal.^cellExponent;
 
-            % Obtain the exponent for the non-linear stage
-            idx = 1+cellIdx(cc);
-            cellExponent = p(idx);
+        % Extract the gain for this cell class and stimulus direction
+        idx = 1 + (cellIdx(cc)-1)*nCells + 3;
+        gain = p(idx);
 
-            % Apply the non-linearity
-            scaleVal = max(abs(double(subs(rfPostRetinal(cc),0:1:100))));
-            rfPostRetinal = rfPostRetinal.^cellExponent;
+        % Gain (plus backing out the scale change induced by the
+        % exponentiation)
+        rfPostRetinal(cc) = (gain/1000)*rfPostRetinal(cc).*(scaleVal/scaleVal^cellExponent);
 
-            % Extract the gain for this cell class and stimulus direction
-            idx = 4 + nCells*nEccs + (cellIdx(cc)-1)*nEccs + ee;
-            gain = p(idx);
-
-            % Gain
-            rfPostRetinal(cc) = (gain/1000)*rfPostRetinal(cc).*(scaleVal/scaleVal^cellExponent);
-
-        end
-
-        % Add the postRetinal RFs together
-        rfPostRetinal = sum(rfPostRetinal);
-
-        % Derive the amplitude and phase from the Fourier model
-        ttfComplex = double(subs(rfPostRetinal,studiedFreqs));
-        amplitude = abs(ttfComplex);
-        phase = unwrap(angle(ttfComplex));
-
-        if any(isnan(amplitude))
-            foo=1;
-        end
-
-        response(ss,ee,:) = amplitude;
     end
+
+    % Add the postRetinal RFs together
+    rfPostRetinal = sum(rfPostRetinal);
+
+    % Derive the amplitude and phase from the Fourier model
+    ttfComplex = double(subs(rfPostRetinal,studiedFreqs));
+    amplitude = abs(ttfComplex);
+    phase = unwrap(angle(ttfComplex));
+
+    responseAtEcc(ss,:) = amplitude;
 end
 
 

060d420001e7db937979d6979a5d4546fa75bc3a
gkaguirre <gkaguirre@me.com>
2024-01-01 15:50:29 -0500

    Getting the CGP stim struct right

--- code/analysis/main_mtSinaiStimulusConstruction_CGP.m
+++ code/analysis/main_mtSinaiStimulusConstruction_CGP.m
@@ -38,8 +38,11 @@ stimTime = {};
 stimLabels = '(stimLabels),{';
 dirLabels = {'LMS','LminusM','S'};
 condLabels = {'f0Hz','f2Hz','f4Hz','f8Hz','f16Hz','f32Hz','f64Hz','attention'};
+seqA = [5 2 4 5 7 2 5 4 2 7 3 6 5 1 2 3 7 6 1 7 1 3 5 6 4 1 1 1];
+seqB = [1 6 6 2 1 5 3 2 2 6 7 4 6 3 3 4 4 3 1 1 4 7 7 5 5 1 1 1];
 freqVals = [0,2,4,8,16,32,64];
 dirOrder = [2 3 1];
+seqVals = {'A','B'};
 dirVals = {'LightFlux','LminusM_wide','S_wide'};
 nConds = length(condLabels);
 nRows = nConds*length(stimFileList);
@@ -47,34 +50,47 @@ offset = 0;
 for ii=1:length(stimFileList)
     load(fullfile(stimFileList(ii).folder,stimFileList(ii).name),'results');
     dirOrderSet{ii} = results.thisDir;
+    switch char([results.trialEvents.stimFreqHz])
+        case char(arrayfun(@(x) freqVals(x),seqA))
+            seqOrderSet{ii} = 'A';
+        case char(arrayfun(@(x) freqVals(x),seqB))
+            seqOrderSet{ii} = 'B';
+        otherwise
+            error('I do not recognize this sequence of stimuli')
+    end
 end
 for dd = 1:length(dirOrder)
-    gg = dirOrder(dd);
-    stimFileIdx = find(strcmp(dirOrderSet,dirVals{gg}));
-    for ii=1:length(stimFileIdx)
-        load(fullfile(stimFileList(stimFileIdx(ii)).folder,stimFileList(stimFileIdx(ii)).name),'results');
-        stimMat = zeros(nRows,nTimeSamples);
-        offset = (ii-1)*nConds+(dd-1)*nConds*12;
-        freqEvents = [results.trialEvents.stimFreqHz];
-        for tt = 1:length(freqEvents)
-            rowIdx = (tt-1)*timeDivs+1;
-            freqIdx = find(freqVals==freqEvents(tt));
-            stimMat(offset+freqIdx,rowIdx:rowIdx+timeDivs) = 1;
-        end
-        stimMat(offset+1,337:end) = 1;
-        attentionEventTimes = [results.attentionEvents.eventTimeSecs];
-        for tt = 1:length(attentionEventTimes)
-            rowIdx = round(attentionEventTimes*attentionEventTimeScale/deltaT);
-            stimMat(offset+nConds,rowIdx) = 1;
-        end
-        stimulus{end+1}=stimMat;
-        thisDir = dirLabels{strcmp(results.thisDir,dirVals)};
-        theseLabels = cellfun(@(x) ['(' x '_' thisDir '_' sprintf('%0.2d',ii) '),'],condLabels,'UniformOutput',false);
-        theseLabels = strcat(theseLabels{:});
-        stimLabels = [stimLabels theseLabels];
+    for ss = 1:length(seqVals)
+        stimFileIdx = find(...
+            strcmp(dirOrderSet,dirVals{dirOrder(dd)})...
+            .* ...
+            strcmp(seqOrderSet,seqVals{ss}) ...
+            );
+        for ii=1:length(stimFileIdx)
+            load(fullfile(stimFileList(stimFileIdx(ii)).folder,stimFileList(stimFileIdx(ii)).name),'results');
+            stimMat = zeros(nRows,nTimeSamples);
+            offset = (ii-1)*nConds+(ss-1)*nConds*6+(dd-1)*nConds*12;
+            freqEvents = [results.trialEvents.stimFreqHz];
+            for tt = 1:length(freqEvents)
+                rowIdx = (tt-1)*timeDivs+1;
+                freqIdx = find(freqVals==freqEvents(tt));
+                stimMat(offset+freqIdx,rowIdx:rowIdx+timeDivs) = 1;
+            end
+            stimMat(offset+1,337:end) = 1;
+            attentionEventTimes = [results.attentionEvents.eventTimeSecs];
+            for tt = 1:length(attentionEventTimes)
+                rowIdx = round(attentionEventTimes*attentionEventTimeScale/deltaT);
+                stimMat(offset+nConds,rowIdx) = 1;
+            end
+            stimulus{end+1}=stimMat;
+            thisDir = dirLabels{strcmp(results.thisDir,dirVals)};
+            theseLabels = cellfun(@(x) ['(' x '_' thisDir '_' sprintf('%0.2d',ii) '),'],condLabels,'UniformOutput',false);
+            theseLabels = strcat(theseLabels{:});
+            stimLabels = [stimLabels theseLabels];
 
-        % Create the stimTime vector
-        stimTime{end+1} = 0:deltaT:(nTimeSamples-1)*deltaT;
+            % Create the stimTime vector
+            stimTime{end+1} = 0:deltaT:(nTimeSamples-1)*deltaT;
+        end
     end
 end
 

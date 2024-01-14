b2bc67af75ce52b2e1fa2a8a487d2759a04a5034
gkaguirre <gkaguirre@me.com>
2024-01-14 08:39:36 -0500

    adjusting threshold params

--- code/figures/FigX_visualFieldTempSens.m
+++ code/figures/FigX_visualFieldTempSens.m
@@ -19,11 +19,11 @@ localDataDir = fullfile(tbLocateProjectSilent('Patterson_2024_JNeurosci'),'data'
 
 % This is the threshold for the goodness of fit to the fMRI time-series
 % data. We only display those voxels with this quality fit or better
-r2Thresh = 0.25;
+r2Thresh = 0.1;
 
 % This is the threshold for the goodness of fit of the Watson model to the
 % TTF in each vertex. We only display those voxels with this fVal or lower
-fValThresh = 1;
+fValThresh = 1.5;
 
 % Prepare the figures
 figHandle = figure('Renderer','painters');
@@ -50,6 +50,8 @@ for ss = 1:length(subjectNames)
     eccenMap = cifti_read(tmpPath); eccenMap = eccenMap.cdata;
     tmpPath = fullfile(localDataDir,[subjectNames{ss} '_resultsFiles'],[subjectNames{ss} '_angle.dscalar.nii']);
     polarMap = cifti_read(tmpPath); polarMap = polarMap.cdata;
+    tmpPath = fullfile(localDataDir,[subjectNames{ss} '_resultsFiles'],[subjectNames{ss} '_sigma.dscalar.nii']);
+    sigmaMap = cifti_read(tmpPath); sigmaMap = sigmaMap.cdata;
 
     % Initialize or load the fitResults
     filePath = fullfile(localDataDir,[subjectNames{ss} '_resultsFiles'],[subjectNames{ss} '_WatsonFit_results.mat']);
@@ -65,18 +67,15 @@ for ss = 1:length(subjectNames)
         fValSet(fValIdx) = cellfun(@(x) x(whichStim), fitResults.fVal(fValIdx));
         goodIdx = find(logical( (results.R2 > r2Thresh) .* (fValSet < fValThresh) .* (vArea == 1) ));
 
-        % Assemble thge components for the field map plot. We assume that
-        % the sigma size (in degrees of visual angle) of each vertex is
-        % equivalent to the eccentricity multiplied by a scale factor of
-        % 0.16, which is derived from Figure 7A of Kay et al 2013,
+        % Assemble thge components for the field map plot.
         % Compressive spatial summation in human visual cortex, J Neurophys
         vals = cellfun(@(x) x(whichStim),fitResults.peakFreq(goodIdx));
         polarVals = polarMap(goodIdx);
         eccenVals = eccenMap(goodIdx);
-        sigmaVals = eccenVals*0.16;
+        sigmaVals = sigmaMap(goodIdx);
         rangeVals = rangeValSet{whichStim};
 
-         nexttile;
+        nexttile;
         createFieldMap(vals,polarVals,eccenVals,sigmaVals,rangeVals);
         title([subjects{ss} ' - ' stimulusDirections{whichStim}]);
 
